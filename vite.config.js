dfd0ca138622a5a9575d089a290164ec7dfb9941
Tony Chan <tony.chan@sciteline.com>
2024-11-09 21:53:05 -0500

    update configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/blood-glucose-control-live-demo",
+  base: "/blood-glucose-control-live-demo/",
   plugins: [react()],
 })
